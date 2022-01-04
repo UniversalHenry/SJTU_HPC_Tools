4eac619aa6ab3a3ddd7dbd0683cc03f7326ce4a0
HenryLu_LHC <40770992+UniversalHenry@users.noreply.github.com>
2022-01-04 19:15:18 +0800

    serial sometimes more efficient

--- always_download.sh
+++ always_download.sh
@@ -1,13 +1,22 @@
 #! /bin/bash
 # always download files
 . ./var.sh
+mkdir $local_download_dir
 mkdir ./tmp
 while true
 do
-    sshpass -p $password \
-    ssh $user@data.hpc.sjtu.edu.cn ls $remote_dir > $rsync_file_list
-    cat $rsync_file_list | xargs --max-args=1 --max-procs=$num_worker --replace=% \
-    sshpass -p $password \
-    rsync -avzur --progress --partial \
-    $user@data.hpc.sjtu.edu.cn:$remote_dir% $local_download_dir
+    if $num_worker==1
+    then
+        sshpass -p $password \
+        rsync -avzur --progress \
+        $user@data.hpc.sjtu.edu.cn:$remote_dir \
+        $local_download_dir 
+    else
+        sshpass -p $password \
+        ssh $user@data.hpc.sjtu.edu.cn ls $remote_dir > $rsync_file_list
+        cat $rsync_file_list | xargs --max-args=1 --max-procs=$num_worker --replace=% \
+        sshpass -p $password \
+        rsync -avzur --progress --partial \
+        $user@data.hpc.sjtu.edu.cn:$remote_dir% $local_download_dir
+    fi
 done
